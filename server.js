5b17d083ddb258be238b50b8451a43216d73685e
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2023-03-17 15:58:59 -0600

    server set up

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3004;
+
+app.get("/", (req, res) => {
+  res.json("Hello, World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
